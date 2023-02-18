aa9649bcbd24eb89854b0bb8b307f6ce7348c8a4
Peter Kure <peterkure256@gmail.com>
2023-02-18 22:07:43 +0300

    new file:   components/Account.js 	new file:   components/Checkout.js 	new file:   components/Home.js 	new file:   components/Login.js 	new file:   components/Signup.js 	new file:   components/Store.js 	modified:   pages/index.js 	new file:   pages/layout/Layout.js

---
+++ components/Login.js
@@ -0,0 +1,10 @@
+export default function Login(){
+    return(
+    <div>
+        <Head>
+            <title>PierreSmithInc</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>PierreSmithInc</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to our Website!" />
         <p className="description">
-          Peter got started on editing <code>pages/index.js</code>
+          Peter and Smith Collection &copy 2023.
         </p>
       </main>
 
